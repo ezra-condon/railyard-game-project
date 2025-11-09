30c2e8ac165479d0268d8e1460354ce76c64c664
Ezra Condon <ezracondon@pm.me>
2025-11-09 14:41:42 -0500

    Created Main, removed LinkedList & Stack
    - Created "Main" class,
    - Removed "Stack" & "LinkedList" to be rewritten.

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    //This is the main class, containing the main method. 
+    //game commands are ran through here.
+    
+    public static void main(String[] args) {
+        
+    }
+}
