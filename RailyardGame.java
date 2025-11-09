729d9a63866525fff8b1bc39f045ed1d979e3c14
Ezra Condon <ezracondon@pm.me>
2025-11-09 16:32:39 -0500

    Updated RailyardGame
    - added destination array
    - added code to create objects necessary for game function (stack, sortNode, track1-3)

--- RailyardGame.java
+++ RailyardGame.java
@@ -1,5 +1,20 @@
 public class RailyardGame {
-    public static void main(String[] args){
-        
-    }
+    //This class serves to act as the game object, initiated by the main method.
+
+    //array of destination strings to be referenced by each node.
+    public static String[] destinations = {"Nanning","Kowloon","Xiamen","Changsha","Nanchang"};
+    
+    //stack storing all cars to be sorted by player
+    public nLinkedList stack = new nLinkedList();
+
+    //Spot where nodes popped from stack to be stored
+    public static Node sortNode = null; 
+
+    //Tracks for sorted cars by player
+    public nLinkedList track1 = new nLinkedList();
+    public nLinkedList track2 = new nLinkedList();
+    public nLinkedList track3 = new nLinkedList();
+
+    
+
 }
